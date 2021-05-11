9107b30fcd29a25810c4ac75ab47c6fbbcc1a889
burtjm <burtjm@gmatc.matc.edu>
2021-05-11 01:35:26 -0500

    Added H1

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,6 +2,8 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import styled from 'styled-components'
+import { H1 } from '../Heading'
+
 
 const Outer = styled.header`
 background: ${props => props.theme.header.backgroundColor};
@@ -12,9 +14,7 @@ margin-bottom:1.45rem;
   max-width: 960px;
   padding: 1.45rem 1.0875rem;
   `
-  const H1 =styled.h1`
-  margin:0px;
-  `
+ 
 const StyledLink = styled(Link)`
 color: white;
 text-decoration: none;
